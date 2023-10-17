90588d5988625d65fc4209f23fbdbef01f6363c8
John Benedict Tiglao Benedicto <jbenedicto@tspi.com.ph>
2023-10-17 12:22:13 +0800

    Add axios to signin and signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -3,14 +3,13 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import http from '../utils/http';
 
 function Copyright(props) {
   return (
@@ -30,6 +29,10 @@ const defaultTheme = createTheme();
 const Signup = () => {
   const handleSubmit = () => {
     //handle signup here
+    http.get('get/users')
+    .then((res) => {
+      console.log(res);
+    })
   }
   return (
     <ThemeProvider theme={defaultTheme}>
