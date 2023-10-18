f8397fed35221ac52df4dfeabbbb09e30e0571ba
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2023-10-18 08:03:30 +0800

    Update: Remove private routing for dashboard

--- client/src/components/routing/Routing.jsx
+++ client/src/components/routing/Routing.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Signin from '../Signin';
 import Signup from '../Signup';
-import PrivateRoute from './PrivateRoute';
 import Dashboard from '../Dashboard';
 
-const Routing = ({user}) => {
+const Routing = () => {
   return (
     <Routes>
         <Route path='/' element={<Signin />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
-        
-        <Route element={<PrivateRoute user={user} />}>
-            <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
+        <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
   )
 }
